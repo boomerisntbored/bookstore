f21147de2f28f61f440b216d7798cb4a9c96b601
Daniel Santana <jpdaniel.santaan@gmail,com>
2025-10-10 15:15:53 -0300

    using the backup

--- api/serializers/product_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from api.models.product import Product
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
